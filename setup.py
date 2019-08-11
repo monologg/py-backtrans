38d8083337c5a4cd7990269b1b0ccb303ed627b8
monologg <adieujw@gmail.com>
2019-08-12 02:34:38 +0900

    add setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='kakaotrans',
+    version='0.1.0',
+    url='https://github.com/monologg/py-backtrans',
+    license='MIT',
+    author='Jangwon Park',
+    author_email='adieujw@gmail.com',
+    description='PyBacktrans: Python library for backtranslation',
+    packages=find_packages(exclude=[]),
+    long_description=open('README.md', 'r', encoding='utf-8').read(),
+    long_description_content_type="text/markdown",
+    python_requires='>=3',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=['googletrans',
+                      'nltk']
+)
